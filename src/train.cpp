05aa7644413501866a5f4b6efa123d468b58e71d
eklochkova101 <260723543+eklochkova101@users.noreply.github.com>
2026-05-31 19:54:22 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,45 @@
-// Copyright 2021 NNTU-CS
+// Copyright 2026 NNTU-CS
 #include "train.h"
+
+Train::Train() : countOp(0), first(nullptr) {}
+
+int Train::getOpCount() {
+  return countOp;
+}
+
+void Train::addCar(bool light) {
+  Car* car = new Car{light, nullptr, nullptr};
+  if (!first) {
+    first = car;
+    car->next = car->prev = car;
+  }
+  else {
+    car->next = first;
+    car->prev = first->prev;
+    first->prev->next = car;
+    first->prev = car;
+  }
+}
+
+int Train::getLength() {
+  if (!first) return 0;
+  countOp = 0;
+  bool startState = first->light;
+  first->light = true;
+  Car* scout = first;
+  while (true) {
+    int dist = 0;
+    do {
+      scout = scout->next;
+      countOp++;
+      dist++;
+    } while (!scout->light);
+    scout->light = false;
+    int backSteps = dist;
+    while (backSteps--) {
+      scout = scout->prev;
+      countOp++;
+    }
+    if (!first->light) return dist;
+  }
+}
